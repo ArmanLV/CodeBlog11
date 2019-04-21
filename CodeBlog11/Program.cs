326239e1f2d514c737e4d85c7b587adce3395fa2
ArmanLV <32594089+ArmanLV@users.noreply.github.com>
2019-04-21 17:01:53 +0300

    Update Program.cs

--- CodeBlog11/Program.cs
+++ CodeBlog11/Program.cs
@@ -6,13 +6,15 @@ namespace CodeBlog11
     {
         static void Main(string[] args)
         {
+            // Получаю данные
             Console.WriteLine("Введите имя");
             string Name = Console.ReadLine();
             Console.WriteLine("Введите силу (от 0 до 100)");
             int Power = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Введите вес");
             int Weight = Convert.ToInt32(Console.ReadLine());
-
+            
+            // Задаю значения, используя перегрузку операторов
             var Person1 = new Swimmer(Name, Power, Weight);
             var Person2 = new Swimmer("Пётр", 40, 65);
             var Person3 = Person1 + Person2;
@@ -24,6 +26,8 @@ namespace CodeBlog11
             var Person9 = Person1 != Person2;
             var Person10 = Person1 > Person2;
             var Person11 = Person1 < Person2;
+            
+            // Вывожу значения
             Console.WriteLine($"{Person1.Name} {Person1.Power} {Person1.Weight}");
             Console.WriteLine($"{Person2.Name} {Person2.Power} {Person2.Weight}");
             Console.WriteLine($"{Person3.Name} {Person3.Power} {Person3.Weight}");
